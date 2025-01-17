e00c0f8afb50aed23c7cdfb34332e5e2dd5a8074
isishome <isishome@hanmail.net>
2025-01-17 12:58:21 +0900

    fixed ads

--- frontend/docs/.vitepress/theme/index.ts
+++ frontend/docs/.vitepress/theme/index.ts
@@ -13,28 +13,22 @@ export default {
         return (!import.meta.env.SSR && window?.innerWidth
           ? window?.innerWidth
           : 0) < 1280
-          ? h(
-              'ClientOnly',
-              h(Adsense, {
-                dataAdSlot: '7595465749',
-                width: 728,
-                height: 90
-              })
-            )
+          ? h(Adsense, {
+              dataAdSlot: '7595465749',
+              width: 728,
+              height: 90
+            })
           : undefined
       },
       'aside-ads-before': () => {
         return (!import.meta.env.SSR && window?.innerWidth
           ? window?.innerWidth
           : 0) >= 1280
-          ? h(
-              'ClientOnly',
-              h(Adsense, {
-                dataAdSlot: '7901796235',
-                width: 160,
-                height: 600
-              })
-            )
+          ? h(Adsense, {
+              dataAdSlot: '7901796235',
+              width: 160,
+              height: 600
+            })
           : undefined
         // https://vitepress.dev/guide/extending-default-theme#layout-slots
       }
