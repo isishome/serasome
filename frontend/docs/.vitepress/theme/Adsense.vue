0f5f88df3e0cee92535203bc1aa832c87b931b54
isishome <isishome@hanmail.net>
2025-01-17 12:32:48 +0900

    fixed ads

--- frontend/docs/.vitepress/theme/Adsense.vue
+++ frontend/docs/.vitepress/theme/Adsense.vue
@@ -36,7 +36,7 @@ const dataAdtest = computed(() =>
 const style = computed(() =>
   props.width && props.height
     ? `display: inline-block; width: ${props.width}px; height: ${props.height}px`
-    : `display: block`
+    : `display: block; min-height: 50px`
 )
 
 const render = () => {
