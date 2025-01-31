215f65e43199b5ab63cd202f913e0bd6ebd574c7
isishome <isishome@hanmail.net>
2025-01-31 18:44:10 +0900

    update ads

--- frontend/docs/.vitepress/theme/useResize.ts
+++ frontend/docs/.vitepress/theme/useResize.ts
@@ -1,8 +1,8 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 
 export function useResize(debounceTime = 200) {
-  const width = ref(window.innerWidth)
-  const height = ref(window.innerHeight)
+  const width = ref(window?.innerWidth ?? 0)
+  const height = ref(window?.innerHeight ?? 0)
   let timeoutId: NodeJS.Timeout
 
   const updateSize = () => {
